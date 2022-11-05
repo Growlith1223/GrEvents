0a931144ff6a45fba34bb5fb347feeaf962b5eff
Growlith1223 <Growlith1223@users.noreply.github.com>
2022-11-05 00:04:01 -0500

    Initial Release

---
+++ include/main.h
@@ -0,0 +1,7 @@
+#ifdef BUILD_DLL
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
+
+EXPORT void wack();
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <main.h>
+#include <iostream>
+
+EXPORT void wack() {
+    std::cout << "Wack\n" << std::endl;
+}
