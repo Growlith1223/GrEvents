67a2f21764f7e6487c47961d0e8a418e575edb3e
Growlith1223 <Growlith1223@users.noreply.github.com>
2023-02-12 09:05:46 -0600

    refactoring

--- include/GrEvents.h
+++ include/GrEvents.h
@@ -6,21 +6,23 @@
 #ifndef H_GREVENTS_H
 #define H_GREVENTS_H
 class EventPacket {
- public:
-  ~EventPacket() {}
+   public:
+    ~EventPacket() {
+    }
 };
 namespace EventManager {
-/**
- * @brief Triggers every listener for a given event
- * @tparam EventType Event to trigger
- * @param args parameters to send to each listener
- */
-EXPORT void signal(const char* EventType, EventPacket&);
-/**
- * @brief Attach function to event
- * @tparam EventType Event to listen to
- * @param func The function we want the EventManager to call
- */
-EXPORT void subscribeTo(const char* EventType, void (*func)(EventPacket&));
+    /**
+     * @brief Triggers every listener for a given event
+     * @param EventType Name of Event to trigger
+     * @param packet parameters to send to each listener packed into a packet
+     * for ease of use
+     */
+    EXPORT void signal(const char* EventType, EventPacket& packet);
+    /**
+     * @brief Attach function to event
+     * @param EventType Name of Event to listen to
+     * @param func The function we want the EventManager to call
+     */
+    EXPORT void subscribeTo(const char* EventType, void (*func)(EventPacket&));
 };  // namespace EventManager
 #endif
