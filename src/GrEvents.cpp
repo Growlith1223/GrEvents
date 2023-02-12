b08ca4d43025bdbaa75a3328e4b9b8dc550ae0b3
Growlith1223 <Growlith1223@users.noreply.github.com>
2023-02-12 09:06:35 -0600

    actually check if the listener exists or not instead of blindly hoping it will.

--- src/GrEvents.cpp
+++ src/GrEvents.cpp
@@ -8,11 +8,27 @@
 typedef void (*func)(EventPacket&);
 std::map<std::string, std::vector<func>> listeners;
 void EventManager::signal(const char* EventType, EventPacket& ep) {
-  std::vector<func>& eventHandler = listeners[EventType];
-  for (func d : eventHandler) d(ep);
+    if (listeners.find(EventType) ==
+        listeners
+            .end()) {  // if not found, let user know that it does not exist.
+        char* err = new char[256];
+        sprintf(err, "Could not find '%s' within the available events.",
+                EventType);
+        throw new std::runtime_error(err);
+    }
+    std::vector<func>& eventHandler =
+        listeners[EventType];  // Retrieve event listener
+    for (func d : eventHandler)
+        d(ep);  // for each observer attached, we signal to execute them.
 }
 
 void EventManager::subscribeTo(const char* EventType, func fn) {
-  std::vector<func>& eventHandler = listeners[EventType];
-  eventHandler.push_back(fn);
+    if (listeners.find(EventType) ==
+        listeners.end()) {  // if not found, we create it
+        listeners.emplace(std::pair<const char*, std::vector<func>>(
+            EventType, std::vector<func>()));
+    }
+    std::vector<func>& eventHandler =
+        listeners[EventType];    // Retrieve event listener
+    eventHandler.push_back(fn);  // add observer to listener.
 }
